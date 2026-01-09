7e939190eb31a4e7e618879c6f8b2f8f2e4a9af4
jonassvedin75 <jonassvedin@gmail.com>
2026-01-09 07:48:38 +0100

    Uppdatera loggor i bildspelet: lägg till Sika, Scanfast och Contractor

--- src/components/logo-carousel.tsx
+++ src/components/logo-carousel.tsx
@@ -1,9 +1,11 @@
 export function LogoCarousel() {
   const logos = [
-    "/byggtema_logo.webp",
-    "/Hedin-Construction-Black_Hedin-Construction-Black.png",
-    "/NP_logo_black.svg",
-    "/OFBygg_Liten.webp",
+    { src: "/Logo_Sika_AG.svg.png", alt: "Sika" },
+    { src: "/scanfast.svg", alt: "Scanfast" },
+    { src: "/contractor.jpg", alt: "Contractor" },
+    { src: "/OFBygg_Liten.webp", alt: "OF Bygg" },
+    { src: "/Hedin-Construction-Black_Hedin-Construction-Black.png", alt: "Hedin Construction" },
+    { src: "/NP_logo_black.svg", alt: "Northpower" },
   ];
 
   return (
@@ -13,23 +15,23 @@ export function LogoCarousel() {
         <div className="marquee-wrapper">
           <div className="marquee-track">
             <div className="marquee-group">
-              {logos.map((src, idx) => (
+              {logos.map((logo, idx) => (
                 <div key={`a-${idx}`} className="marquee-item">
-                  <img src={src} alt={`Logotyp ${idx + 1}`} width={160} height={48} loading="eager" className="logo-img" />
+                  <img src={logo.src} alt={logo.alt} width={160} height={48} loading="eager" className="logo-img" />
                 </div>
               ))}
             </div>
             <div className="marquee-group" aria-hidden="true">
-              {logos.map((src, idx) => (
+              {logos.map((logo, idx) => (
                 <div key={`b-${idx}`} className="marquee-item">
-                  <img src={src} alt={`Logotyp ${idx + 1}`} width={160} height={48} loading="eager" className="logo-img" />
+                  <img src={logo.src} alt={logo.alt} width={160} height={48} loading="eager" className="logo-img" />
                 </div>
               ))}
             </div>
             <div className="marquee-group" aria-hidden="true">
-              {logos.map((src, idx) => (
+              {logos.map((logo, idx) => (
                 <div key={`c-${idx}`} className="marquee-item">
-                  <img src={src} alt={`Logotyp ${idx + 1}`} width={160} height={48} loading="eager" className="logo-img" />
+                  <img src={logo.src} alt={logo.alt} width={160} height={48} loading="eager" className="logo-img" />
                 </div>
               ))}
             </div>
